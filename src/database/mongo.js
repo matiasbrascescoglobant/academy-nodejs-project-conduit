1a13b23ddff3d588c9fa3e22877f531c51fcad21
matiasbrascescoglobant <matias.brascesco@globant.com>
2021-12-30 10:18:28 -0300

    The database is dropped when starting the development environment so that the self-tests run smoothly.

--- src/database/mongo.js
+++ src/database/mongo.js
@@ -5,7 +5,12 @@ dotenv.config();
 
 const log = debug('globant:db');
 const uri = process.env.MONGO_URI;
+const environment = process.env.NODE_ENV || 'default';
 
 mongoose.connect(uri, {}, () =>{
     log('Database connection is ready.');
+    log(`${environment} environment`);
+    if(environment === 'dev'){
+        mongoose.connection.db.dropDatabase();
+    }
 });
