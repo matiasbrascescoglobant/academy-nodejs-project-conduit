88ba13fbf9b6ad5950807df27d56e0062e085bb7
matiasbrascescoglobant <matias.brascesco@globant.com>
2021-12-31 21:28:40 -0300

    Refactored profiles model

--- src/models/profiles-model.js
+++ src/models/profiles-model.js
@@ -2,7 +2,8 @@ import mongoose from 'mongoose';
 const Schema = mongoose.Schema;
 
 const ProfileSchema = new Schema({
-    user: { type: Schema.Types.ObjectId, ref: "user" }
+    follower: { type: Schema.Types.ObjectId, ref: "user" },
+    following: { type: Schema.Types.ObjectId, ref: "user" }
 }, {
     timestamps: true,
     versionKey: false
